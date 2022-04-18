f5fc9d42f5d26cbc86500862e7a9db744c875b9f
Alexandra Long <ael2203@columbia.com>
2022-04-17 23:57:03 -0400

    Added button to cocktail game but it needs a template.

--- UIDProj1-main/Cocktail/cocktails/server.py
+++ UIDProj1-main/Cocktail/cocktails/server.py
@@ -3,16 +3,16 @@ from flask import render_template
 from flask import Response, request, jsonify
 app = Flask(__name__)
 
-howtovids = {
+drinks = {
     "1" :{
-        "id": "1",
+        "id": "tequilasunrise",
         "title": "Tequila Sunrise",
         "video": "https://www.youtube.com/embed/_8gXKNqU9Mc",
         "ingredients": ["1 1/2 ounces of Tequila", "3/4 cup Orange juice", "3/4 ounces Grenadine", "1 Maraschino cherry"],
         "images": ["https://thumbs.dreamstime.com/b/bottle-mexican-tequila-white-background-stock-image-150427683.jpg", "https://assets.sainsburys-groceries.co.uk/gol/7736185/1/640x640.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROTiRh3FUZAb119bd88bOeZPjlZGKjLYxaJC3SwCA5rrp9PhsW0ghqAafQcgak-dmZL-c&usqp=CAU", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRYd5eI92Lat-g-0L6OVMJ0jhZFUPIGNDZ0lA&usqp=CAU"]
     },
     "2": {
-        "id": "2",
+        "id": "ginandtonic",
         "title": "Gin and Tonic",
         "video": "https://cdn.jwplayer.com/videos/aOvVQdyT-tXzwfO7V.mp4",
         "ingredients": ["2 ounces Gin", "4 ounces Tonic Water"],
@@ -20,14 +20,13 @@ howtovids = {
 
     },
     "3": {
-        "id": "3",
+        "id": "margarita",
         "title": "Margarita",
         "video": "https://www.youtube.com/embed/6F6LdutFKyM",
         "ingredients": ["1 ounce Cointreau", "2 ounces Lime Juice", "3 ounces Tequila"],
         "images": [""]
     }
 }
-
 quizzes = {
     "1" : {
         "1": "1",
@@ -45,8 +44,8 @@ def homepage():
 
 @app.route('/howtovid/<id>')
 def howto(id):
-    global howtovids
-    drink = howtovids[id]
+    global drinks
+    drink = drinks[id]
     return render_template('howto.html', drink = drink)
 
 @app.route('/quiz/<id>')
@@ -59,49 +58,6 @@ def quiz(id):
 def display():
     return render_template('quiz_landing.html')
 
-@app.route('/search_results')
-def search_results():
-
-    global results
-    global search
-    global match
-    length = len(results)
-    return render_template('search.html', results=results, search=search, match=match, length=length)
-@app.route('/view/search', methods=['GET','POST'])
-@app.route('/search', methods=['GET','POST'])
-def search():
-    global movies
-    global results
-    global search
-    global match
-    match = {}
-    results = {}
-    json_data = request.get_json()
-    search = json_data.lower()
-    print(search)
-    if (search == ""):
-        return render_template('search.html', results=results, match=match)
-    else:
-        for key in movies:
-            for i in range(len(movies[key]['actors'])):
-                if search in (movies[key]['actors'][i]).lower():
-                    results[key] = movies[key]
-                    match[key] = movies[key]['actors'][i]
-            if search in (movies[key]['title']).lower():
-                results[key] = movies[key]
-                match[key] = movies[key]['title']
-            elif search in (movies[key]['year']).lower():
-                results[key] = movies[key]
-                match[key] = movies[key]['year']
-        print(results)
-        print(match)
-        return render_template('search.html', results=results, match=match)
-
-#@app.route('/add', methods=['GET','POST'])
-#def addData():
- #   global movies
-  #  json_data = request.get_json()
-    
 # ajax for people.js
 if __name__ == '__main__':
     app.run(debug=True)
