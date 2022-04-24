13613b33ebff732fa1295ee12d5a8903ebd097f5
Alexandra Long <ael2203@columbia.com>
2022-04-23 23:35:35 -0400

    Made the draggable in jquery instead of the harded work I had in html template.

--- UIDProj1-main/Cocktail/static/cocktailgame.js
+++ UIDProj1-main/Cocktail/static/cocktailgame.js
@@ -20,21 +20,90 @@ function countdown() {
       if (minutes == 0 && seconds == 0) clearInterval(interval);
   }, 1000);
 }
+let images = [
+    "https://www.nicepng.com/png/full/85-859751_orange-juice-bottle-clipart.png",
+    "https://www.nicepng.com/png/full/174-1746111_clip-art-royalty-free-stock-collection-of-free.png",
+    "https://www.nicepng.com/png/full/100-1000748_roses-sweetened-lime-juice.png",
+    "https://www.nicepng.com/png/full/293-2939694_tonic-and-licorice-ledgers-tonic-water.png",
+    "https://www.nicepng.com/png/full/67-678248_750-ml-agave-organic-nectar-monin-vanilla-syrup.png",
+    "https://www.nicepng.com/png/full/849-8497547_camarena-silver-tequila-camarena-tequila.png",
+    "https://www.nicepng.com/png/full/638-6385672_bombay-sapphire-london-dry-gin-bombay-sapphire-gin.png",
+    "https://www.nicepng.com/png/full/21-219751_limes-wedges-cuts-chopped-wedge-of-lime.png",
+    "https://www.nicepng.com/png/full/703-7037179_gabriel-boudier-bartender-curacao-triple-sec-liqueur.png",
+    "https://www.nicepng.com/png/full/8-80183_cherry-free-download-png-red-cherries-fridge-magnet.png"
+]
+let items = [
+    "orange",
+    "grenadine",
+    "limejuice",
+    "tonic",
+    "simple",
+    "tequila",
+    "gin",
+    "lime",
+    "triplesec",
+    "cherry"
+]
 
+function makeDrag(items) {
+    $(".leftside").empty()
+    $(".rightside").empty()
+    $(".leftT").empty()
+    $(".leftM").empty()
+    $(".leftB").empty()
+    $(".rightT").empty()
+    $(".rightM").empty()
+    $(".rightB").empty()
+    $(".leftside").append("<div class='row leftT'></div>")
+    $(".leftside").append("<div class='row leftM'></div>")
+    $(".leftside").append("<div class='row leftB'></div>")
+    $(".rightside").append("<div class='row rightT'></div>")
+    $(".rightside").append("<div class='row rightM'></div>")
+    $(".rightside").append("<div class='row rightB'></div>")
+    $(".leftT").append("<div class='leftside labelpad'>Orange Juice &nbsp;&nbsp;&nbsp; Grenadine</div>")
+    $(".leftM").append("<div class='leftside labelpad'>Lime Juice</div>")
+    $(".leftB").append("<div class='leftside labelpad'>Tonic Water &nbsp;&nbsp;&nbsp; Simple Syrup</div>")
+    $(".rightT").append("<div class='rightside labelpad'>Tequila &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; Gin</div>")
+    $(".rightM").append("<div class='rightside labelpad limelabel'>Lime</div>")
+    $(".rightB").append("<div class=' labelpad'>Triple Sec &nbsp;&nbsp;&nbsp;&nbsp; Cherry</div>")
+   
+
+    $.each(items, function(key, value) {
+        $("." + value).hover(function() {
+            $(this).draggable({revert: "valid"})
+        })
+        console.log(key)
+        if (key < 2) {
+            let elem = $("<img class="+value+" src='"+images[key]+"' alt='"+value+"'>")
+            $(".leftT").append(elem)
+            elem.draggable({revert: "invalid"})
+        } else if (key == 2) {
+            let elem = $("<img class="+value+" src='"+images[key]+"' alt='"+value+"'>")
+            $(".leftM").append(elem)
+            elem.draggable({revert: "invalid"})
+        } else if (key > 2 && key < 5) {
+            let elem = $("<img class="+value+" src='"+images[key]+"' alt='"+value+"'>")
+            $(".leftB").append(elem)
+            elem.draggable({revert: "invalid"})
+        } else if (key > 4 && key < 7) {
+            let elem = $("<img class="+value+" src='"+images[key]+"' alt='"+value+"'>")
+            $(".rightT").append(elem)
+            elem.draggable({revert: "invalid"})
+        } else if (key == 7) {
+            let elem = $("<img class="+value+" src='"+images[key]+"' alt='"+value+"'>")
+            $(".rightM").append(elem)
+            elem.draggable({revert: "invalid"})
+        } else if (key > 7 && key < 10) {
+            let elem = $("<img class="+value+" src='"+images[key]+"' alt='"+value+"'>")
+            $(".rightB").append(elem)
+            elem.draggable({revert: "invalid"})
+        }
+    })
+}
 $(document).ready(function() {
     $('.js-timeout').text("2:00");
     countdown();
-    $('.orange').draggable({revert: "valid"});
-    $('.grenadine').draggable({revert: "valid"});
-    $('.limejuice').draggable({revert: "valid"});
-    $('.tonic').draggable({revert: "valid"});
-    $('.simple').draggable({revert: "valid"});
-    $('.tequila').draggable({revert: "valid"});
-    $('.gin').draggable({revert: "valid"});
-    $('.lime').draggable({revert: "valid"});
-    $('.triplesec').draggable({revert: "valid"});
-    $('.cherry').draggable({revert: "valid"});
-
+    makeDrag(items);
     $('.beaker').droppable({
         drop: function(event,ui) {
             dialog.dialog("open");
@@ -47,9 +116,12 @@ $(document).ready(function() {
         buttons: {
             "Yes": function(e) {
                 console.log("Pressed Yes!");
+                $(this).dialog("close");
+                makeDrag(items);
             },
             Cancel: function() {
                 $(this).dialog("close");
+                makeDrag(items);
             }
         }
     });
