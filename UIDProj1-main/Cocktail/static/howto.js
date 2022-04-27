68826ada6830f1957cf8524b5815a2649f64a87a
Alexandra Long <ael2203@columbia.com>
2022-04-27 08:58:37 -0400

    Updated ingredient list to be more visible and so that the user can't move on until they've checked all boxes.

--- UIDProj1-main/Cocktail/static/howto.js
+++ UIDProj1-main/Cocktail/static/howto.js
@@ -1,18 +1,20 @@
+let n = 0
 $(document).ready(function(){
-
-    let i_list = $("<div>")
+    let t = drink["ingredients"].length
+    $(".ingredients").empty()
+    $(".ingredients").append("<div class='inglabel center tiny'>Check off the ingredient you learn as you watch the video!</div>")
     $.each(drink["ingredients"], function(index,value){
-        let check = $('<input type="checkbox">')
-        let name = "i".concat(String(index))
-        $(check).attr("name", name)
-        $(check).attr("id", name)
-        $(i_list).append(check)
-        let lab = $("<label>")
-        console.log(lab)
-        $(lab).attr("for",name)
-        $(i_list).append(lab)
-        $(lab).html(value)
-        $(i_list).append("<br>")
+        let item = $("<div class='inglabel ingpad center' ><input type='checkbox' id="+index+" class='check' value="+value+"<label for="+index+">&nbsp;"+value+"</label> </div>")
+        $(".ingredients").append(item)
+        $("#"+index).change(function(){
+            if ($("#"+index+':checked').length == $("#"+index).length) {
+               n += 1
+               if (n == t){
+                $('#game').prop('disabled', false);
+               }
+               console.log(n)
+               console.log(t)
+            }
+        });
     })
-    $("#ingredients").append(i_list)
 })
